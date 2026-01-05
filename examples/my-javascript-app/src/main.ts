69856dbcd9e014a0e6e32f3b14bc53f2a494715c
yamachu <yamachu.dev@gmail.com>
2026-01-05 20:19:07 +0900

    dotnetのprojectのindex.htmlの内容を移して、main.jsもdotnetWasm.jsに移植

--- examples/my-javascript-app/src/main.ts
+++ examples/my-javascript-app/src/main.ts
@@ -1,24 +1,16 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+// @ts-expect-error: 型定義がないけど、問題ない
+import { setupDotnet } from "./dotnetWasm.js";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <h1>Stopwatch</h1>
+  <p>
+    Time elapsed in .NET is <span id="time"><i>loading...</i></span>
+  </p>
+  <p>
+    <button id="pause">Pause</button>
+    <button id="reset">Reset</button>
+  </p>
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupDotnet();
